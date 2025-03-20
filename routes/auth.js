6ce5ea88c820f9ec6df7074fdd050ca2b90f91c5
Vimlendu Sharma <vimendusharma@gmail.com>
2025-03-20 14:38:12 +0530

    Remove React imports from backend

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const { use } = require("react");
+//const { use } = require("react");
 
 //generate_JWT
 const generateToken = (id) => {
